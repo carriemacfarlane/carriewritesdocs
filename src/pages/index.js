3826cf87c84c3b9bbfcbd258cd775589bb622749
Carrie Macfarlane <carrie.m.macfarlane@gmail.com>
2024-05-05 16:41:14 -0400

    New home page layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import picOfCarrie from '/img/porch-300px.jpeg';
 
 import styles from './index.module.css';
 
@@ -22,13 +23,13 @@ function HomepageHeader() {
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div> */}
-        <div className="flex flex-row">
-          <div>
-            <h1 className="text-red">Hi, I'm Carrie.</h1>
-            <h2>I’m an organized self-starter who knows how to do research. I'll ask informed questions, and I'll finish your work on time.</h2>
+        <div className="flex flex-row p-8 gap-8">
+          <div className="w-1/2 flex flex-col justify-center">
+            <p className="mb-8 text-5xl font-black">Hi, I'm Carrie.</p>
+            <p>I’m an organized self-starter who knows how to do research. I'll ask informed questions, and I'll finish your work on time.</p>
           </div>
-          <div>
-            hello
+          <div className="w-1/2">
+            <img src={picOfCarrie} />
           </div>
         </div>
       </div>
